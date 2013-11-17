3a88051b643bfdb9fffb63570f98f85826150d59
raghunayyar <me@iraghu.com>
2013-11-17 11:32:41 +0530

    Initial Commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('angularUsefulsApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
